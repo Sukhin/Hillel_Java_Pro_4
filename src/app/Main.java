0f0e89e1142c0daf4f66022e14eed8f976a29e80
Vitaly_Sukhin <VVS11101992@gmail.com>
2026-05-08 22:53:04 +0300

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,11 @@
+package app;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the program for entering and displaying customers data!\n");
+        Scanner enter = new Scanner(System.in);
+    }
+}
